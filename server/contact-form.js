8bf362e4afbd2c4a822ab15c00479f0c56c0ec6f
Emily Chen <emily@apportable.com>
2015-03-16 19:31:43 -0700

    fixed error

--- server/contact-form.js
+++ server/contact-form.js
@@ -2,17 +2,16 @@ Meteor.methods({
   sendEmail: function(data) {
     check(data, Schema.contactForm);
 
-    var text = "Name: " + data.name + "\n\n"
-    + "Email: " + data.email + "\n\n\n\n"
-    + "Subject: " + data.subject + "\n\n"
-    + data.message;
+    var text = data.name + " - "
+    + data.email + "\n\n\n\n"
+    + "\n\n" + data.message;
 
     this.unblock();
 
     Email.send({
       to: Meteor.settings.contactForm.emailTo,
       from: data.email,
-      subject: "Website Contact Form - Message From " + data.name,
+      subject: Meteor.settings.contactForm.emailSubject(data),
       text: text
     });
   }
