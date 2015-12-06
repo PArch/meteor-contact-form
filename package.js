b972415cc7300ba055e4c00fd7d9cc768cf12c67
juniobranco <Br@anco04>
2015-12-06 17:07:38 +0800

    Updating dependencies

--- package.js
+++ package.js
@@ -2,6 +2,7 @@ Package.describe({
   name: 'branco:contact-form',
   summary: 'A cotact form with reCaptcha implementation to prevent spam',
   version: '1.0.0',
+  documentation:"README.md",
   git: 'https://github.com/tenshiemi/meteor-contact-form'
 });
 
@@ -10,8 +11,8 @@ Package.onUse(function(api) {
   api.use('email', 'server');
   api.use('templating', 'client');
   api.use('aldeed:autoform@5.5.1');
-  api.addFiles('contact-form.js');
   api.use('appshore:recaptcha@2.0.6');
+  api.addFiles('contact-form.js');
   api.addFiles([
     'client/views/contact-form.html',
     'client/views/contact-form.js',
